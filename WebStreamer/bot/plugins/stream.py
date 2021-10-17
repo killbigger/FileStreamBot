470f601b19003b2da0e2787472e76795361f6bfa
killbigger <55308851+killbigger@users.noreply.github.com>
2021-10-17 20:28:40 +0530

    await removed

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -82,7 +82,7 @@ async def private_receive_handler(c: Client, m: Message):
 <b>🚸 Nᴏᴛᴇ : Tʜɪs ᴘᴇʀᴍᴀɴᴇɴᴛ Lɪɴᴋ, Nᴏᴛ Exᴘɪʀᴇᴅ</b>\n
 <i>© @SebinDavis </i>"""
         print('posted get request to linode server')
-        r = await requests.get('http://172.105.37.7:3000/download/{}'.format(log_msg.message_id))
+        r =  requests.get('http://172.105.37.7:3000/download/{}'.format(log_msg.message_id))
         await log_msg.reply_text(text=f"**RᴇQᴜᴇꜱᴛᴇᴅ ʙʏ :** [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n**Uꜱᴇʀ ɪᴅ :** `{m.from_user.id}`\n**Dᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋ :** {stream_link}", disable_web_page_preview=True, parse_mode="Markdown", quote=True)
         await m.reply_text(
             text=msg_text.format(file_name, file_size, stream_link),
